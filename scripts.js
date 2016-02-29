84a364ba450e5ba623c6500911ed6dbf3bf8997e
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-02-29 09:05:16 -0600

    Add text

--- scripts.js
+++ scripts.js
@@ -4,17 +4,22 @@ var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
     'itant eate anot eat nato inate eat anot tain eat',
     'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
-$("#block").append(sentences[0]);
+
+
+$(document).ready(function () {
+    $("#block").text(sentences[0]);
+});
+
 
 $(document).bind('keydown', function (event) {
    
     //If you want to check the code from the key
     console.log(event.keyCode);
     var keydown = event.keyCode;
-    if(keydown== 20){
+    if (keydown == 20) {
         console.log("CHANGE KEYBOARD");
         if ($("#keyboard-upper-container").attr("class") == 'text-center keyboard-container active') {
-            $("#keyboard-upper-container").removeClass("active"); 
+            $("#keyboard-upper-container").removeClass("active");
             $("#keyboard-upper-container").addClass("disable");
             $('#keyboard-lower-container').removeClass("disable");
             $('#keyboard-lower-container').addClass("active");
@@ -24,38 +29,60 @@ $(document).bind('keydown', function (event) {
             $('#keyboard-lower-container').removeClass("active");
             $('#keyboard-lower-container').addClass("disable");
         }
-  
+
     }
     switch (event.keyCode) {
-        case 48:
-            $('span#0').addClass('active-style');
+        case 192:
+            $('span#126').addClass('active-style');
+            $('span#96').addClass('active-style');
             break;
         case 49:
             $('span#1').addClass('active-style');
+            $('span#33').addClass('active-style');
             break;
         case 50:
             $('span#2').addClass('active-style');
+            $('span#64').addClass('active-style');
             break;
         case 51:
             $('span#3').addClass('active-style');
+            $('span#35').addClass('active-style');
             break;
         case 52:
             $('span#4').addClass('active-style');
+            $('span#36').addClass('active-style');
             break;
         case 53:
             $('span#5').addClass('active-style');
+            $('span#37').addClass('active-style');
             break;
         case 54:
             $('span#6').addClass('active-style');
+            $('span#94').addClass('active-style');
             break;
         case 55:
             $('span#7').addClass('active-style');
+            $('span#38').addClass('active-style');
             break;
         case 56:
             $('span#8').addClass('active-style');
+            $('span#42').addClass('active-style');
             break;
         case 57:
             $('span#9').addClass('active-style');
+            $('span#40').addClass('active-style');
+            break;
+        case 48:
+            $('span#0').addClass('active-style');
+            $('span#41').addClass('active-style');
+            break;
+        case 189:
+            $('span#45').addClass('active-style');
+            $('span#95').addClass('active-style');
+            break;
+        case 187:
+            $('span#61').addClass('active-style');
+            $('span#43').addClass('active-style');
             break;
         case 81:
             $('span#q').addClass('active-style');
@@ -97,6 +124,18 @@ $(document).bind('keydown', function (event) {
             $('span#p').addClass('active-style');
             $('span#P').addClass('active-style');
             break;
+        case 219:
+            $('span#123').addClass('active-style');
+            $('span#91').addClass('active-style');
+            break;
+        case 221:
+            $('span#125').addClass('active-style');
+            $('span#93').addClass('active-style');
+            break;
+        case 220:
+            $('span#124').addClass('active-style');
+            $('span#92').addClass('active-style');
+            break;
         case 65:
             $('span#a').addClass('active-style');
             $('span#A').addClass('active-style');
@@ -133,6 +172,14 @@ $(document).bind('keydown', function (event) {
             $('span#l').addClass('active-style');
             $('span#L').addClass('active-style');
             break;
+        case 186:
+            $('span#59').addClass('active-style');
+            $('span#58').addClass('active-style');
+            break;
+        case 222:
+            $('span#39').addClass('active-style');
+            $('span#34').addClass('active-style');
+            break;
         case 90:
             $('span#z').addClass('active-style');
             $('span#Z').addClass('active-style');
@@ -161,12 +208,24 @@ $(document).bind('keydown', function (event) {
             $('span#m').addClass('active-style');
             $('span#M').addClass('active-style');
             break;
+        case 188:
+            $('span#44').addClass('active-style');
+            $('span#60').addClass('active-style');
+            break;
+        case 190:
+            $('span#46').addClass('active-style');
+            $('span#62').addClass('active-style');
+            break;
+        case 191:
+            $('span#47').addClass('active-style');
+            $('span#63').addClass('active-style');
+            break;
         case 32:
             $('div#32').addClass('active-style');
             break;
- 
-   }
-   
+
+    }
+
 });
 
 $(document).bind('keyup', function (event) {
@@ -175,35 +234,57 @@ $(document).bind('keyup', function (event) {
     console.log(event.keyCode);
     var keyup = event.keyCode;
     switch (event.keyCode) {
-        case 48:
-            $('span#0').removeClass('active-style');
+        case 192:
+            $('span#126').removeClass('active-style');
+            $('span#96').removeClass('active-style');
             break;
         case 49:
             $('span#1').removeClass('active-style');
+            $('span#33').removeClass('active-style');
             break;
         case 50:
             $('span#2').removeClass('active-style');
+            $('span#64').removeClass('active-style');
             break;
         case 51:
             $('span#3').removeClass('active-style');
+            $('span#35').removeClass('active-style');
             break;
         case 52:
             $('span#4').removeClass('active-style');
+            $('span#36').removeClass('active-style');
             break;
         case 53:
             $('span#5').removeClass('active-style');
+            $('span#37').removeClass('active-style');
             break;
         case 54:
             $('span#6').removeClass('active-style');
+            $('span#94').removeClass('active-style');
             break;
         case 55:
             $('span#7').removeClass('active-style');
+            $('span#38').removeClass('active-style');
             break;
         case 56:
             $('span#8').removeClass('active-style');
+            $('span#42').removeClass('active-style');
             break;
         case 57:
             $('span#9').removeClass('active-style');
+            $('span#40').removeClass('active-style');
+            break;
+        case 48:
+            $('span#0').removeClass('active-style');
+            $('span#41').removeClass('active-style');
+            break;
+        case 189:
+            $('span#45').removeClass('active-style');
+            $('span#95').removeClass('active-style');
+            break;
+        case 187:
+            $('span#61').removeClass('active-style');
+            $('span#43').removeClass('active-style');
             break;
         case 81:
             $('span#q').removeClass('active-style');
@@ -245,6 +326,18 @@ $(document).bind('keyup', function (event) {
             $('span#p').removeClass('active-style');
             $('span#P').removeClass('active-style');
             break;
+        case 219:
+            $('span#123').removeClass('active-style');
+            $('span#91').removeClass('active-style');
+            break;
+        case 221:
+            $('span#125').removeClass('active-style');
+            $('span#93').removeClass('active-style');
+            break;
+        case 220:
+            $('span#124').removeClass('active-style');
+            $('span#92').removeClass('active-style');
+            break;
         case 65:
             $('span#a').removeClass('active-style');
             $('span#A').removeClass('active-style');
@@ -281,6 +374,14 @@ $(document).bind('keyup', function (event) {
             $('span#l').removeClass('active-style');
             $('span#L').removeClass('active-style');
             break;
+        case 186:
+            $('span#59').removeClass('active-style');
+            $('span#58').removeClass('active-style');
+            break;
+        case 222:
+            $('span#39').removeClass('active-style');
+            $('span#34').removeClass('active-style');
+            break;
         case 90:
             $('span#z').removeClass('active-style');
             $('span#Z').removeClass('active-style');
@@ -309,27 +410,21 @@ $(document).bind('keyup', function (event) {
             $('span#m').removeClass('active-style');
             $('span#M').removeClass('active-style');
             break;
+        case 188:
+            $('span#44').removeClass('active-style');
+            $('span#60').removeClass('active-style');
+            break;
+        case 190:
+            $('span#46').removeClass('active-style');
+            $('span#62').removeClass('active-style');
+            break;
+        case 191:
+            $('span#47').removeClass('active-style');
+            $('span#63').removeClass('active-style');
+            break;
         case 32:
             $('div#32').removeClass('active-style');
             break;
     }
 
-});
-
-/*$("#target").keypress(function () {
-    var keyPress = event.keyCode;
-    console.log("keypress() called." + keyPress);
-    switch (event.keyCode) {
-        case 65:
-            $("a").style.css( "border", "3px solid red" );
-            break;
-    }
-});*/
-
-
-$("#hide").click(function () {
-    $("p").hide();
-});
-$("#show").click(function () {
-    $("p").show();
 });
